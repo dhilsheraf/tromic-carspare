88bd8e4f3cd50bad7afe0e275d78c36182dd88b0
dhilsheraf <dhilsherafummer@gmail.com>
2024-11-06 00:17:29 +0530

    category started

--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -3,14 +3,16 @@ const Admin = require("../models/adminModel")
 const express = require('express');
 const router = express();
 
-router.get("/login",adminController.loadAdminLogin)
-router.post("/login",adminController.adminLogin)
+router.get("/",adminController.loadAdminLogin)
+router.post("/",adminController.adminLogin)
 router.get("/dashboard",adminController.loadAdminDashboard)
 
 // users 
 router.get("/users",adminController.loadUsers)
 router.post('/users/:action/:id',adminController.blockunblock)
 
+//category
+router.get("/category",adminController.loadCategory)
 
 
 module.exports = router
